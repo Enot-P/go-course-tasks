7d3d48788dbff90862ab6aa877a40593b0bf2c56
Enot-P <nikita.ptashckin@yandex.ru>
2026-03-18 00:02:26 +0300

    task 3.2.2 complete
    task 3.2.2 recomplete

--- 03-types/3.2-methods/main.go
+++ 03-types/3.2-methods/main.go
@@ -11,7 +11,23 @@ func (b Book) Summary() string {
 	return fmt.Sprintf("%s (%d pages)", b.Title, b.Pages)
 }
 
+type Wallet struct {
+	Balance int
+}
+
+func (w *Wallet) Deposit(amount int) {
+	w.Balance += amount
+}
+
 func main() {
 	// task 3.2.1
 	fmt.Printf("%s", Book{Title: "title", Pages: 12}.Summary()) // title (12 pages)
+
+	// task 3.2.2
+	wallet := Wallet{Balance: 0}
+	wallet.Deposit(1)
+	wallet.Deposit(1)
+	wallet.Deposit(1)
+	wallet.Deposit(1)
+	fmt.Printf("\n\nwallet: %d\n", wallet.Balance) // wallet: 4
 }
