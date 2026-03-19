71e184cad79c353f123865e6bf9f7603ef2c565d
Enot-P <nikita.ptashckin@yandex.ru>
2026-03-19 18:22:45 +0300

    task 3.3.2 complete

--- 03-types/3.3-interfaces/main.go
+++ 03-types/3.3-interfaces/main.go
@@ -23,6 +23,27 @@ func printGreeting(g Greeter) string {
 	return g.Greet()
 }
 
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Square struct {
+	Side float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (s Square) Area() float64 {
+	return s.Side * s.Side
+}
+
 func main() {
 	// task 3.3.1
 	user := User{}
@@ -30,4 +51,13 @@ func main() {
 
 	fmt.Println(printGreeting(user))
 	fmt.Println(printGreeting(guest))
+
+	// task 3.3.2
+	rectangle := Rectangle{Width: 10, Height: 15}
+	sqare := Square{Side: 5}
+	shapes := []Shape{rectangle, sqare}
+
+	for _, shape := range shapes {
+		fmt.Println(shape.Area())
+	}
 }
