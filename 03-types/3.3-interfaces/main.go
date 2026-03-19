ac7dc070af501701efc342e6e3467b8634687468
Enot-P <nikita.ptashckin@yandex.ru>
2026-03-19 19:10:35 +0300

    task 3.3.4 complete

--- 03-types/3.3-interfaces/main.go
+++ 03-types/3.3-interfaces/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Greeter interface {
 	Greet() string
@@ -54,26 +57,56 @@ func (a Athlete) Run() string {
 	return "Athlete is running"
 }
 
+type Starter interface {
+	Start()
+}
+type Stopper interface {
+	Stop()
+}
+type Machine interface {
+	Starter
+	Stopper
+}
+type Engine struct{}
+
+func (e Engine) Start() {
+	fmt.Println("Machine is starting")
+}
+
+func (e Engine) Stop() {
+	fmt.Println("Machine is stopped")
+}
+
+func runCycle(m Machine) {
+	m.Start()
+	time.Sleep(1 * time.Second)
+	m.Stop()
+}
+
 func main() {
 	// task 3.3.1
 	user := User{}
 	guest := Guest{}
 
-	fmt.Println(printGreeting(user))
-	fmt.Println(printGreeting(guest))
+	fmt.Println(printGreeting(user))  // Hi from User
+	fmt.Println(printGreeting(guest)) // Hi from Guest
 
 	// task 3.3.2
 	rectangle := Rectangle{Width: 10, Height: 15}
 	sqare := Square{Side: 5}
 	shapes := []Shape{rectangle, sqare}
 
-	for _, shape := range shapes {
-		fmt.Println(shape.Area())
+	for _, shape := range shapes { // 150
+		fmt.Println(shape.Area()) // 25
 	}
 
 	// task 3.3.3
 	athlete := Athlete{}
-	fmt.Println(athlete.Run())
+	fmt.Println(athlete.Run()) // Athlete is running
 
 	var _ Runner = Athlete{} // Компилится, в чем смысл?
+
+	// task 3.3.4
+	bmwEngine := Engine{} // Machine is starting
+	runCycle(bmwEngine)   // Machine is stopped
 }
