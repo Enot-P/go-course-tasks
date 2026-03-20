9d022786c46aa552adbda58682c66e712ccf1a83
Enot-P <nikita.ptashckin@yandex.ru>
2026-03-21 01:05:07 +0300

    task 3.5.5 complete

--- 03-types/3.5-generics/main.go
+++ 03-types/3.5-generics/main.go
@@ -38,18 +38,26 @@ func IndexOf[T comparable](items []T, target T) int {
 	return -1
 }
 
+type Pair[K comparable, V any] struct {
+	Key   K
+	Value V
+}
+
 func main() {
 	fmt.Println(Echo(1))
 	fmt.Println(Echo(true))
 	fmt.Println(Echo("Hi"))
 	fmt.Println()
 
+	fmt.Println("/////task 3.5.2/////")
 	// task 3.5.2
 	slice1 := make([]int, 0)
 	fmt.Println(First(slice1))
 	slice1 = append(slice1, 1)
 	fmt.Println(First(slice1))
 
+	fmt.Println("")
+	fmt.Println("/////task 3.5.3/////")
 	// task 3.5.3
 	intSlice := []int{1, 2, 3, 4, 5, 6}
 	floatSlice := []float64{1.2, 2.7, 3.2, 4.0, 4.5}
@@ -58,7 +66,23 @@ func main() {
 	fmt.Println(Sum(floatSlice)) // 15.600000000000001
 	fmt.Println("")
 
+	fmt.Println("/////task 3.5.4/////")
 	// task 3.5.4
 	fmt.Println(IndexOf(intSlice, 3))  // 2
 	fmt.Println(IndexOf(intSlice, 30)) // -1
+
+	fmt.Println("")
+	fmt.Println("/////task 3.5.5/////")
+
+	// task 3.5.5
+	firstPair := Pair[string, int]{
+		Key:   "Key",
+		Value: 101,
+	}
+	secondPair := Pair[int, string]{
+		Key:   11001,
+		Value: "Value",
+	}
+	fmt.Println(firstPair)
+	fmt.Println(secondPair)
 }
