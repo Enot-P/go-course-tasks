2dbcd469cec690f7e194cbb1cf7ba3c0ec168d42
Enot-P <nikita.ptashckin@yandex.ru>
2026-03-21 00:43:14 +0300

    task 3.5.2 complete

--- 03-types/3.5-generics/main.go
+++ 03-types/3.5-generics/main.go
@@ -9,8 +9,23 @@ func Echo[T any](v T) T {
 	return v
 }
 
+func First[T any](items []T) (T, bool) {
+	if len(items) == 0 {
+		var zero T
+		return zero, false
+	}
+	return items[0], true
+}
+
 func main() {
 	fmt.Println(Echo(1))
 	fmt.Println(Echo(true))
 	fmt.Println(Echo("Hi"))
+	fmt.Println()
+
+	// task 3.5.2
+	slice1 := make([]int, 0)
+	fmt.Println(First(slice1))
+	slice1 = append(slice1, 1)
+	fmt.Println(First(slice1))
 }
