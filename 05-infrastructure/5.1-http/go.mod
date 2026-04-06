7cedeb1e28fccfb3a1c8f6db54cec7a5dfa3f113
Enot-P <nikita.ptashckin@yandex.ru>
2026-04-07 00:51:21 +0300

    task 5.1.1 complete

---
+++ 05-infrastructure/5.1-http/go.mod
@@ -0,0 +1,3 @@
+module Enot-P/go-course-tasks/task5
+
+go 1.26.1
---
+++ 05-infrastructure/5.1-http/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("ok"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, err := w.Write([]byte(err.Error()))
+			if err != nil {
+				fmt.Println("Can't send response: ", err)
+			}
+		}
+	})
+
+	log.Println("http server on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
