8c8448673fcb1f73e3ac941cc1799e2f7e9a9e0c
Enot-P <nikita.ptashckin@yandex.ru>
2026-04-07 04:06:27 +0300

    task 5.1.4 and 5.1.5 complete

--- 05-infrastructure/5.1-http/main.go
+++ 05-infrastructure/5.1-http/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -25,8 +30,30 @@ func main() {
 
 	mux.HandleFunc("POST /api/v1/tokens", TokenHand)
 
-	log.Println("http server on :8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
-		log.Fatal(err)
+	server := http.Server{
+		Addr:              ":8080",
+		Handler:           mux,
+		ReadHeaderTimeout: 2 * time.Second,
+		ReadTimeout:       5 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       30 * time.Second,
+	}
+
+	go func() {
+		log.Println("http server on :8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("graceful Shutdown failed: %v", err)
 	}
 }
